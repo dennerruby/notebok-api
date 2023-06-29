467a9304f3db1952201728a8ddc797cc51621715
Dennerruby <dennersk891@gmail.com>
2023-06-29 09:49:16 -0300

    config crud and model andess

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,9 @@
 class Contact < ApplicationRecord
   belongs_to :kind, optional: true
   has_many :phones
+  has_one :address
 
+  accepts_nested_attributes_for :address
   accepts_nested_attributes_for :phones, allow_destroy: true
   # def author
   #   "Teste Author"
