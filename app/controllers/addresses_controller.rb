d624946d52d05a3e8ddaf71004492980c295ff09
Dennerruby <dennersk891@gmail.com>
2023-09-14 22:51:28 -0300

    adjusted routes

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,14 +1,39 @@
 class AddressesController < ApplicationController
-  before_action :address
+  before_action :set_contact
 
- # GET /address/1
+    def destroy
+      @contact.address.destroy
+    end
+
+    def create
+      @contact.address = Address.new(address_params)
+
+      if @contact.save
+        render json: @contact.address, status: :created, location: contact_address_url(@contact)
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @contact.address.update(address_params)
+        render json: @contact.address
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    end
+    # GET /address/1
     def show
-      render json: @address
+      render json: @contact.address
     end
 
     private
     # Use callbacks to share common setup or constraints between actions.
-    def address
-      @address = Contact.find(params[:contact_id]).address
+    def set_contact
+      @contact = Contact.find(params[:contact_id])
+    end
+
+    def address_params
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
     end
 end
